bdf54b25c1e50ba2e6b9ce713d534552e39ee514
Kev2308 <kevinpaulchief@gmail.com>
2023-07-30 11:18:33 +0530

    Added more recommendations
    Added 15 more to list compared to 5 earlier
    updated requirements file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pickle
 import streamlit as st
 import requests
 
+
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=03aef64156abe242348c245be3698b05&language=en-US".format(movie_id)
     data = requests.get(url)
@@ -15,7 +16,7 @@ def recommend(movie):
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
-    for i in distances[1:6]:
+    for i in distances[1:21]:
         # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
@@ -36,14 +37,15 @@ selected_movie = st.selectbox(
 
 if st.button('Show Recommendation'):
     recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+
     col1, col2, col3, col4, col5 = st.columns(5)
+
     with col1:
         st.text(recommended_movie_names[0])
         st.image(recommended_movie_posters[0])
     with col2:
         st.text(recommended_movie_names[1])
         st.image(recommended_movie_posters[1])
-
     with col3:
         st.text(recommended_movie_names[2])
         st.image(recommended_movie_posters[2])
@@ -53,7 +55,52 @@ if st.button('Show Recommendation'):
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
-
-
-
-
+    
+    with col1:
+        st.text(recommended_movie_names[5])
+        st.image(recommended_movie_posters[5])
+    with col2:
+        st.text(recommended_movie_names[6])
+        st.image(recommended_movie_posters[6])
+    with col3:
+        st.text(recommended_movie_names[7])
+        st.image(recommended_movie_posters[7])
+    with col4:
+        st.text(recommended_movie_names[8])
+        st.image(recommended_movie_posters[8])
+    with col5:
+        st.text(recommended_movie_names[9])
+        st.image(recommended_movie_posters[9])
+    
+    with col1:
+        st.text(recommended_movie_names[10])
+        st.image(recommended_movie_posters[10])
+    with col2:
+        st.text(recommended_movie_names[11])
+        st.image(recommended_movie_posters[11])
+    with col3:
+        st.text(recommended_movie_names[12])
+        st.image(recommended_movie_posters[12])
+    with col4:
+        st.text(recommended_movie_names[13])
+        st.image(recommended_movie_posters[13])
+    with col5:
+        st.text(recommended_movie_names[14])
+        st.image(recommended_movie_posters[14])
+    
+    with col1:
+        st.text(recommended_movie_names[15])
+        st.image(recommended_movie_posters[15])
+    with col2:
+        st.text(recommended_movie_names[16])
+        st.image(recommended_movie_posters[16])
+    with col3:
+        st.text(recommended_movie_names[17])
+        st.image(recommended_movie_posters[17])
+    with col4:
+        st.text(recommended_movie_names[18])
+        st.image(recommended_movie_posters[18])
+    with col5:
+        st.text(recommended_movie_names[19])
+        st.image(recommended_movie_posters[19])
+    
